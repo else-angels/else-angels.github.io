f7c4726ff8d28904d6d06e9ace9843b8a7fdb095
Martin Boudreau <mboudrea\cstj.qc.ca>
2023-09-04 23:08:37 -0400

    Mise en page sommaire

---
+++ src/components/Nav.vue
@@ -0,0 +1,21 @@
+<template>
+	<nav id="nav-principal">
+		<ul>
+			<li>
+				<a href="">Accueil</a>
+			</li>
+			<li>
+				<a href="">Accueil</a>
+			</li>
+			<li>
+				<a href="">Accueil</a>
+			</li>
+			<li>
+				<a href="">Accueil</a>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<script setup>
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,7 @@
+<template>
+<h1>Index</h1>
+</template>
+<script setup>
+import Signature from '../components/Signature.vue';
+</script>
+
